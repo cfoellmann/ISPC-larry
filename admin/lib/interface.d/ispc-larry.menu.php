cc1137ff178e59bd19c61c79332ebf988151f833
Christian Foellmann <foellmann@foe-services.de>
2012-07-19 10:02:25 +0200

    added basis files for theme options in the frontend

---
+++ admin/lib/interface.d/ispc-larry.menu.php
@@ -0,0 +1,6 @@
+<?php
+$items[] = array(   'title'     => 'ISPC-larry Theme',
+                    'target' 	=> 'content',
+                    'link'	=> 'tools/ispc-larry_settings.php',
+                    'html_id'   => 'ispc-larry_settings');
+?>
