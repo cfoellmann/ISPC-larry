bf29026ea767ce568dfe50c776cd49cbb8e6ffd9
Christian Foellmann <foellmann@foe-services.de>
2012-07-20 20:17:04 +0200

    correction of menu and language files

--- admin/lib/interface.d/tpl_ispc-larry.menu.php
+++ admin/lib/interface.d/tpl_ispc-larry.menu.php
@@ -1,6 +1,6 @@
 <?php
 $items[] = array(   'title'     => 'ISPC-larry Theme',
                     'target' 	=> 'content',
-                    'link'	=> 'tools/tpl_ispc-larry_admin.php',
+                    'link'	=> 'admin/tpl_ispc-larry_admin.php',
                     'html_id'   => 'tpl_ispc-larry_admin');
 ?>
